2d3cc2bc6facfdf82ce9bf8bf6c7c289a60d6e9c
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-11-05 10:10:40 +0100

    chore: remove all project files and dependencies for a fresh start

--- src/components/ui/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ text, disabled = false, onClick }) => {
-  return (
-    <button disabled={disabled} className="primary-button" onClick={onClick}>
-      {text}
-    </button>
-  );
-};
-
-export default Button;
